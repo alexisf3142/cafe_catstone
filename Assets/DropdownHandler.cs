491a063b84fecf0b1dbdecfbbd3d3f09f015f21d
Michael-Foley <59493450+Zahz1@users.noreply.github.com>
2022-05-01 13:45:39 -0400

    Working Dropdown for saves

--- Assets/DropdownHandler.cs
+++ Assets/DropdownHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class DropdownHandler : MonoBehaviour
 {
@@ -15,8 +16,19 @@ public class DropdownHandler : MonoBehaviour
 
         List<string> items = new List<string>();
         items.Add("Choose Save");
-        items.Add("Save 1");
-        items.Add("Save 2");
+        string path = Application.dataPath + Path.AltDirectorySeparatorChar;
+        var files = System.IO.Directory.GetFiles(path);
+        foreach (string file in files)
+        {
+            //Do work on the files here
+            if (file.EndsWith("SaveData.json"))
+            {
+                string tempString = file.Remove(0, path.Length);
+                string extraData = "SaveData.json";
+                string cafeName = tempString.Remove(tempString.IndexOf(extraData));
+                items.Add(cafeName); 
+            }
+        }
 
         foreach (var item in items)
         {
@@ -31,7 +43,12 @@ public class DropdownHandler : MonoBehaviour
         int index = dropdown.value;
 
         selctedItem = dropdown.options[index].text;
-        Debug.Log(selctedItem);
+        //Debug.Log(selctedItem);
+    }
+
+    public string getSelectedSave()
+    {
+        return selctedItem;
     }
 
     // Update is called once per frame
